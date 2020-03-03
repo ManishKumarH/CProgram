0760319bc9f6f5033fdaa761857d7b0a02cb52b0
ManishKumarH <61144053+ManishKumarH@users.noreply.github.com>
2020-03-03 10:59:29 +0530

    Add files via upload

---
+++ cc.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+int age;
+printf("enter age \n");
+scanf("%d",&age);
+if(age>=16 && age<=20)
+{
+printf("you are teenager");
+}
+else
+{
+printf("you are not teenager");
+}}
